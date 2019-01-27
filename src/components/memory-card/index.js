c4ca9881aafee36f6e05bc596701bcf9b17b03d2
Luciano da Silva Martins <zicadopv@gmail.com>
2019-01-27 10:30:42 -0200

    Fim da aula 24

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,32 +1,32 @@
 function createMemoryCard() {
-  const $memoryCard = document.createElement("article");
-
-  const $iconCollab = `
-  <img
-  src='img/icon-collabcode.png'
-  alt='Geuio mascote da collabcode'
-  class='icon'/>
+  const $memoryCard = `
+    <article class="memory-card">
+      <img
+        src='img/icon-collabcode.png'
+        alt='Geuio mascote da collabcode'
+        class='icon'
+        onClick="handleClick()"
+      />
+    </article>
   `;
-
-  $memoryCard.classList.add('memory-card');
-  $wrapCards.insertBefore($memoryCard, null);
-
-  $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+  return $memoryCard;
 }
 
 function createMemoryCardFront(){
-  const $memoryCardFront = document.createElement("article");
-
-  const $iconC = `
-  <img
-  src='img/icon-c.png'
-  alt='Geuio mascote da collabcode'
-  class='icon'/>
+  const $memoryCard = `
+    <article class="memory-card -front">
+      <img
+        src='img/icon-c.png'
+        alt='Geuio mascote da collabcode'
+        class='icon'
+        onClick="handleClick()"
+      />
+    </article>
   `;
 
-  $memoryCardFront.classList.add("memory-card");
-  $memoryCardFront.classList.add("-front");
-  $wrapCards.insertBefore($memoryCardFront, null);
+  return $memoryCard;
+}
 
-  $memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
+function handleClick() {
+  console.log("ae");
 }

ed4e9a21ba9e58f9ea2a41b834684c654bb3ce85
Luciano da Silva Martins <zicadopv@gmail.com>
2019-02-15 09:32:55 -0200

    Fim da aula 32

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,123 +1,133 @@
-const memoryCard = () =>  {
-  const $head = document.querySelector("head");
-  const $style = document.createElement("style");
+const memoryCard = (function() {
+  const create = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
 
-  $style.textContent = `
-    .memory-card {
-      width: 155px;
-      height: 155px;
-      position: relative;
+    $style.textContent = `
+      .memory-card {
+        width: 155px;
+        height: 155px;
+        position: relative;
+      }
+
+      .memory-card .card {
+        width: 100%;
+        height: 100%;
+        background-color: #f25a70;
+        border-radius: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+        position: relative;
+        cursor: pointer;
+        position: absolute;
+      }
+
+      .memory-card.-active .card,
+      .memory-card.-score .card {
+        display: none;
+      }
+
+      .memory-card.-active .card.-front,
+      .memory-card.-score .card.-front {
+        display: flex;
+      }
+
+      .memory-card .card.-front {
+        background-color: #fff;
+        display: flex;
+      }
+
+      .memory-card .card.-front::before {
+        content: "";
+        width: 95px;
+        height: 95px;
+        background-color: #d4d4d4;
+        border-radius: 50%;
+        position: absolute;
+      }
+
+      .memory-card .card > .icon {
+        width: 100px;
+        height: 100px;
+      }
+
+      .memory-card .card.-front > .icon {
+        position: absolute;
+        transform: translateY(-12px);
+      }
+    `;
+
+    $head.insertBefore($style, null)
+
+    return ({ src, alt, nameClass } ) => `
+      <div class="memory-card" onClick="memoryCard.handleClick(this)">
+        <article class="card -front">
+          <img
+            src="${src}"
+            alt="${alt}"
+            class="icon"
+          />
+        </article>
+        <article class="card">
+          <img
+            src="img/icon-collabcode.png"
+            alt="O mascote da Collabcode o Gueio"
+            class="icon"
+          />
+        </article>
+      </div>
+    `
+  };
+
+  const handleClick = $component => {
+    if (!$component.classList.contains("-active")) {
+      activeMemoryCard($component);
+      checkSure();
     }
 
-    .memory-card .card {
-      width: 100%;
-      height: 100%;
-      background-color: #f25a70;
-      border-radius: 30px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
-      position: relative;
-      cursor: pointer;
-      position: absolute;
+  };
+
+  const activeMemoryCard = $component => {
+    if (qtdActiveMemoryCard < 2){
+      $component.classList.add("-active")
     }
 
-    .memory-card.-active .card,
-    .memory-card.-score .card {
-      display: none;
-    }
+  };
 
-    .memory-card.-active .card.-front,
-    .memory-card.-score .card.-front {
-      display: flex;
-    }
+  const checkSure = () => {
+    if (qtdActiveMemoryCard === 1 ){
+      const $activeMemoryCards = document.querySelectorAll(
+        ".memory-card.-active"
+      );
 
-    .memory-card .card.-front {
-      background-color: #fff;
-      display: flex;
-    }
-
-    .memory-card .card.-front::before {
-      content: "";
-      width: 95px;
-      height: 95px;
-      background-color: #d4d4d4;
-      border-radius: 50%;
-      position: absolute;
-    }
-
-    .memory-card .card > .icon {
-      width: 100px;
-      height: 100px;
-    }
-
-    .memory-card .card.-front > .icon {
-      position: absolute;
-      transform: translateY(-12px);
-    }
-  `;
-
-  $head.insertBefore($style, null)
-
-  return ({ src, alt, nameClass } ) => `
-    <div class="memory-card" onClick="handleClick(this)">
-      <article class="card -front">
-        <img
-          src="${src}"
-          alt="${alt}"
-          class="icon"
-        />
-      </article>
-      <article class="card">
-        <img
-          src="img/icon-collabcode.png"
-          alt="O mascote da Collabcode o Gueio"
-          class="icon"
-        />
-      </article>
-    </div>
-  `;
-};
-
-const handleClick = $component => {
-  if (!$component.classList.contains("-active")) {
-    activeMemoryCard($component);
-    checkSure();
-  }
-};
-
-function activeMemoryCard($component) {
-  if (qtdActiveMemoryCard < 2){
-    $component.classList.add("-active")
-  }
-}
-
-function checkSure() {
-  if (qtdActiveMemoryCard === 1 ){
-    const $activeMemoryCards = document.querySelectorAll(
-      ".memory-card.-active"
-    );
-
-    if(
-      $activeMemoryCards[0].querySelector(".-front .icon").getAttribute("src") ==
-      $activeMemoryCards[1].querySelector(".-front .icon").getAttribute("src")
-    )
-    {
-      store.score++;
-      console.log("Score:", store.score);
-      $activeMemoryCards.forEach( $memoryCard => {
-        $memoryCard.classList.add("-score");
-        $memoryCard.classList.remove("-active");
-      })
-    } else {
-      setTimeout(() => {
+      if(
+        $activeMemoryCards[0].querySelector(".-front .icon").getAttribute("src") ==
+        $activeMemoryCards[1].querySelector(".-front .icon").getAttribute("src")
+      )
+      {
+        store.score++;
+        console.log("Score:", store.score);
         $activeMemoryCards.forEach( $memoryCard => {
+          $memoryCard.classList.add("-score");
           $memoryCard.classList.remove("-active");
-        });
+        })
+      } else {
+        setTimeout(() => {
+          $activeMemoryCards.forEach( $memoryCard => {
+            $memoryCard.classList.remove("-active");
+          });
 
-        qtdActiveMemoryCard = 0;
-      }, 1500);
+          qtdActiveMemoryCard = 0;
+        }, 1500);
+      }
     }
-  }
-}
+
+  };
+
+  return {
+    create,
+    handleClick
+  };
+})();
